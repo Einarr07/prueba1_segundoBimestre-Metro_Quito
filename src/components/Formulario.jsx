5d8bcefe0e8991d7edea9b5008070e85461751b4
dilan-flores <dilan.flores@epn.edu.ec>
2023-08-14 16:32:43 -0500

    Corrección de Actualizar

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -2,9 +2,8 @@ import { v4 as uuidv4} from "uuid"
 import Mensajes from "./Mensajes"
 import { useState } from "react"
 import { useEffect } from 'react'
-import { Formik, Form, Field, ErrorMessage } from "formik";
 
-export const Formulario = ({ setEstado, idMetro }) => {
+export const Formulario = ({ setEstado, idMetro, setIdMetro}) => {
     const [error, setError] = useState(false);
     const [mensaje, setMensaje] = useState(false);
     const [form, setForm] = useState({
@@ -16,12 +15,6 @@ export const Formulario = ({ setEstado, idMetro }) => {
         detalles: ""
     });
 
-    const handleChange = async (e) => {
-        setform({...form,
-            [e.target.name]:e.target.value
-        })
-    }
-
     useEffect(() => {
         if (idMetro) {
             (async function (idMetro) {
@@ -29,15 +22,15 @@ export const Formulario = ({ setEstado, idMetro }) => {
                     const respuesta = await (
                         await fetch(`http://localhost:3000/metro/${idMetro}`)
                     ).json();
-                    const{
+                    const {
                         id,
                         nombre,
                         sector,
                         salida,
                         llegada,
                         maquinista,
-                        detalles
-                    } = respuesta 
+                        detalles,
+                    } = respuesta;
                     setForm({
                         ...form,
                         nombre,
@@ -55,139 +48,168 @@ export const Formulario = ({ setEstado, idMetro }) => {
         }
     }, [idMetro]);
 
-  
-
-    const handleFormSubmit = async (data, {resetForm}) => {
-        if (Object.values(data).some((value) => value === "")) {
-            setError(true);
+    const handleChange = (e) => { 
+        setForm({
+            ...form,
+            [e.target.name]: e.target.value.trim()
+        });
+    }
+    
+    const handleSubmit = async(e)=>{
+        //impide que se pierdan los datos con la actualización de página
+        e.preventDefault()
+        // validadción de campos vacíos
+        if (Object.values(form).includes("") || Object.entries(form).length === 0)
+        {
+            setError(true)
             setTimeout(() => {
-                setError(false);
+                setError(false)
             }, 1000);
-            return;
+            return
         }
         try {
-            if (idMetro) {
-                const url = `http://localhost:3000/metro/${idMetro}`;
-                await fetch(url, {
-                    method: "PUT",
-                    body: JSON.stringify(data),
-                    headers: { "Content-Type": "application/json" }
-                });
-                setEstado(true);
-            } else {
-                const url = "http://localhost:3000/metro";
-                const newData = { ...data, id: uuidv4() };
-                await fetch(url, {
-                    method: "POST",
-                    body: JSON.stringify(newData),
-                    headers: { "Content-Type": "application/json" }
-                });
-                setMensaje(true);
-                setEstado(true);
+            if(form.id){
+                const url = `http://localhost:3000/metro/${form.id}`
+                await fetch(url,{
+                    method:'PUT',
+                    body:JSON.stringify(form),
+                    headers:{'Content-Type':'application/json'}
+                })
+                setEstado(true)
+                setForm({})
+                setIdMetro(null);
+				setTimeout(() => {
+                    setEstado(false)
+                    setForm({})
+                }, 1000)
             }
-
-            setTimeout(() => {
-                setMensaje(false);
-                setEstado(false);
-                resetForm();
-            }, 1000);
+            else{
+                const url ="http://localhost:3000/metro"
+				form.id = uuidv4()
+                await fetch(url,{
+                    // Método para crear un nuevo recurso
+                    method:'POST',
+                    // Convierte el formulario en JSON
+                    body:JSON.stringify(form),
+                    headers:{'Content-Type':'application/json'}
+                })
+                setMensaje(true)
+			    setEstado(true)
+                setTimeout(() => {
+                    setMensaje(false)
+				    setEstado(false)
+                    //setform({})
+                }, 1000);
+            }
         } catch (error) {
             console.log(error);
         }
-    };
 
+    }
+    console.log("Id antes del return:", idMetro);
+    console.log("form antes del return:", form);
     return (
-        <Formik initialValues={form} onSubmit={handleFormSubmit}>
-            <Form>
+        //<Formik initialValues={form} onSubmit={handleFormSubmit}>
+        <form onSubmit={handleSubmit}>
                 {error && <Mensajes tipo="bg-red-900">Existen campos vacíos</Mensajes>}
                 {mensaje && <Mensajes tipo="bg-green-900">Registro exitoso</Mensajes>}
                 <Mensajes tipo={"bg-red-900"}>Validar campos</Mensajes>
 
                 <div>
-                    <label htmlFor='nombre' className='text-gray-700 uppercase font-bold text-sm'>
-                        Nombre:
-                    </label>
-                    <Field
-                        type="text"
-                        id="nombre"
-                        name="nombre"
-                        className={`border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md mb-5`}
-                        placeholder='nombre de la ruta'
-                    /><ErrorMessage name="nombre" component="div" className="text-red-500 mt-1" />
-                </div>
-
-                <div>
-                    <label htmlFor="sector" className='text-gray-700 uppercase font-bold text-sm'>
-                        Sector:
-                    </label>
-                    <Field
-                        type="text"
-                        id="sector"
-                        name="sector"
-                        className={`border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md mb-5`}
-                        placeholder='sector de la ruta'
-                    /><ErrorMessage name="sector" component="div" className="text-red-500 mt-1" />
-                </div>
-
-                <div>
-                    <label htmlFor="salida" className='text-gray-700 uppercase font-bold text-sm'>
-                        Punto de salida:
-                    </label>
-                    <Field
-                        type="text"
-                        id="salida"
-                        name="salida"
-                        className={`border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md mb-5`}
-                        placeholder='punto de salida'
-                    /><ErrorMessage name="salida" component="div" className="text-red-500 mt-1" />
-                </div>
-
-                <div>
-                    <label htmlFor="llegada" className='text-gray-700 uppercase font-bold text-sm'>
-                        Punto de llegada:
-                    </label>
-                    <Field
-                        type="text"
-                        id="llegada"
-                        name="llegada"
-                        className={`border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md mb-5`}
-                        placeholder='punto de llegada'
-                    /><ErrorMessage name="llegada" component="div" className="text-red-500 mt-1" />
-                </div>
-
-                <div>
-                    <label htmlFor='maquinista' className='text-gray-700 uppercase font-bold text-sm'>
-                        Nombre del maquinista:
-                    </label>
-                    <Field
-                        type="text"
-                        id="maquinista"
-                        name="maquinista"
-                        className={`border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md mb-5`}
-                        placeholder='nombre del maquinista'
-                    /><ErrorMessage name="maquinista" component="div" className="text-red-500 mt-1" />
-                </div>
-
-                <div>
-                    <label htmlFor='detalles' className='text-gray-700 uppercase font-bold text-sm'>
-                        Detalles:
-                    </label>
-                    <Field
-                        as="textarea"
-                        id="detalles"
-                        name="detalles"
-                        className={`border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md mb-5`}
-                        placeholder='Detalles de la ruta'
-                    /><ErrorMessage name="detalles" component="div" className="text-red-500 mt-1" />
-                </div>
-
+                <label
+                    htmlFor='nombre'
+                    className='text-gray-700 uppercase font-bold text-sm'>Nombre: </label>
                 <input
+                    id='nombre'
+                    type="text"
+                    className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md mb-5'
+                    placeholder='nombre de la ruta'
+                    name='nombre'
+                    value={form.nombre || ""} 
+                    onChange={handleChange}
+                />
+            </div>
+
+            <div>
+                <label
+                    htmlFor='sector'
+                    className='text-gray-700 uppercase font-bold text-sm'>Sector: </label>
+                <input
+                    id='sector'
+                    type="text"
+                    className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md mb-5'
+                    placeholder='sector de la ruta'
+                    name='sector'
+                    value={form.sector || ""}
+                    onChange={handleChange}
+                />
+            </div>
+
+            <div>
+                <label
+                    htmlFor='salida'
+                    className='text-gray-700 uppercase font-bold text-sm'>Punto de salida: </label>
+                <input
+                    id='salida'
+                    type="text"
+                    className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md mb-5'
+                    placeholder='punto de salida'
+                    name='salida'
+                    value={form.salida || ""}
+                    onChange={handleChange}
+                />
+            </div>
+
+            <div>
+                <label
+                    htmlFor='llegada'
+                    className='text-gray-700 uppercase font-bold text-sm'>Punto de llegada: </label>
+                <input
+                    id='llegada'
+                    type="text"
+                    className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md mb-5'
+                    placeholder='punto de llegada'
+                    name='llegada'
+                    value={form.llegada || ""}
+                    onChange={handleChange}
+                />
+            </div>
+
+            <div>
+                <label
+                    htmlFor='maquinista'
+                    className='text-gray-700 uppercase font-bold text-sm'>Nombre del maquinista: </label>
+                <input
+                    id='maquinista'
+                    type="text"
+                    className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md mb-5'
+                    placeholder='nombre del maquinista'
+                    name='maquinista'
+                    value={form.maquinista || ""}
+                    onChange={handleChange}
+                />
+            </div>
+            <div>
+                <label
+                    htmlFor='detalles'
+                    className='text-gray-700 uppercase font-bold text-sm'>Detalles: </label>
+                <textarea
+                    id='detalles'
+                    type="text"
+                    className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md mb-5'
+                    name='detalles'
+                    value={form.detalles || ""}
+                    onChange={handleChange}
+                />
+            </div>
+
+            <input
                 type="submit"
                 className='bg-sky-900 w-full p-3 
-                text-white uppercase font-bold rounded-lg 
-                hover:bg-red-900 cursor-pointer transition-all'
-                value={form.id ? "Actualizar ruta" : "Registrar ruta"} />
-            </Form >
-        </Formik>
+              text-white uppercase font-bold rounded-lg 
+              hover:bg-red-900 cursor-pointer transition-all'
+              value={form.id ? "Actualizar ruta" : "Registrar ruta"} />
+        </form>
+        //</Formik>
     );
 };
